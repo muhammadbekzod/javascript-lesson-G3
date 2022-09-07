43f32b1720c871b4162b3da144b9518dabe1cf24
Muhammad <61837014+BekzodAbdulkhaev@users.noreply.github.com>
2022-09-07 22:54:29 +0900

    G3 javascript lesson

---
+++ index.js
@@ -0,0 +1,72 @@
+
+
+// a = 10
+// b = 3
+// c = a / b
+
+// d = 9.123675762354761236784798
+// console.log(b ** 3)
+// console.log("ParseInt: ", Number.parseInt(c))
+// console.log("ParseFloat: ", Number.parseFloat(c))
+
+// math = "2 + 2"
+
+// result = 2 + 2
+
+// console.log(math)
+// console.log(result)
+
+// console.log(d)
+// console.log(typeof d)
+
+// console.log(d.toFixed(2))
+// console.log(Number.parseFloat(d.toFixed(2)))
+// console.log(typeof d.toFixed(2))
+
+// console.log(d)
+// 1- masala
+// d = 4
+// p = 3.14
+// L = p * d
+// console.log(L)
+
+
+//Math 
+//Math.round
+// a = 10
+// b = 3
+// c = a / b
+// console.log(Math.round(c))
+
+//Math.pow
+// console.log(Math.pow(5, 3))
+
+// Math.sqrt 
+// console.log(Math.sqrt(25))
+
+
+//Math.cbrt
+
+// console.log(Math.cbrt(125))
+
+//Math.abs
+
+// console.log(Math.abs(25 - 32))
+
+
+
+//Math.ceil 
+
+// console.log(Math.ceil(1.9))
+
+//Math.floor
+
+// console.log(Math.floor(1.9))
+
+// console.log(Math.min(1, 2, 5, 8, 3))
+
+// console.log(Math.max(1, 2, 5, 8, 3))
+
+
+console.log(parseInt(Math.random() * 5))
+
