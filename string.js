aca1baf2aa4eab44340cb7fceee03f53ecd666a1
Muhammad <61837014+BekzodAbdulkhaev@users.noreply.github.com>
2022-09-11 23:27:46 +0900

    string

---
+++ string.js
@@ -0,0 +1,49 @@
+
+// var number = 89
+
+// var binarynumber = 01110001110
+
+// console.log(parseInt(1011001, 2))
+
+// "dsiufhiuwehfiuwe "
+// 'iudwgeifuweuifhuiwef'
+//     `neoisjfioesaiofn`
+var str = `welcome to Be Coder Academy       `
+// var str3 = `Azizbek ${name}`
+// var str2 = `Jobirbek ${name}`
+// var str = `Saydullo ${name}`
+
+// console.log(str.length)
+
+// console.log(str.includes('to'))
+// console.log(str.search('to'))
+// console.log(str.slice(8, 13))
+// console.log(str.substring(8, 13))
+// console.log(str.substr(8, 6))
+
+// console.log(str)
+// console.log(str.replace('coder', 'IT'))
+
+// console.log(str.replace(/Coder/i, 'IT'))
+// console.log(str.replace(/Coder/ig, 'IT'))
+
+// console.log(str.padEnd(15, '*'))
+// console.log(str.padStart(15, '*'))
+
+// console.log(str.startsWith('w'))
+
+// console.log(str.endsWith('Academy'))
+
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
+
+// console.log(str.trim())
+
+var math = "2 + 2"
+
+// console.log(eval(math))
+
+
+console.log(new String(math))
+
+
